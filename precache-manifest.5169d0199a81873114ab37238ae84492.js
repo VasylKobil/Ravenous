a5aa893944edac75006df129879aee067b429235
Vasyl Kobil <vkobil@wp.pl>
2020-09-09 08:53:32 +0200

    Updates

---
+++ precache-manifest.5169d0199a81873114ab37238ae84492.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a893308c6684b5f189dd66d869af8384",
+    "url": "/Ravenous/index.html"
+  },
+  {
+    "revision": "ee5e3f9e2f495ca81266",
+    "url": "/Ravenous/static/css/main.95564dde.chunk.css"
+  },
+  {
+    "revision": "44b17a0c356c7e732238",
+    "url": "/Ravenous/static/js/2.4f2af0f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Ravenous/static/js/2.4f2af0f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee5e3f9e2f495ca81266",
+    "url": "/Ravenous/static/js/main.c5ec7e8f.chunk.js"
+  },
+  {
+    "revision": "cd33480439a159f358bd",
+    "url": "/Ravenous/static/js/runtime-main.9bb2f265.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/Ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/Ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Ravenous/static/media/logo.5d5d9eef.svg"
+  }
+]);
